2b65ab80d86011d9ffcc315240667d63386a7700
Jared Sanson <jared@jared.geek.nz>
2014-09-23 17:32:59 +1200

    Initial commit

---
+++ DllIconHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using SharpShell.SharpIconHandler;
+using SharpShell.Attributes;
+
+//http://sharpshell.codeplex.com/documentation
+
+namespace shellhandler
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".dll")]
+    class DllIconHandler : SharpIconHandler
+    {
+        protected override Icon GetIcon(bool smallIcon, uint iconSize)
+        {
+            //  The icon we'll return.
+            Icon icon = null;
+
+            //  Check the assembly name. If it's a native dll, this'll throw an exception.
+            try
+            {
+                //  SelectedItemPath is provided by 'SharpIconHandlder' and contains the path of the file.
+                AssemblyName.GetAssemblyName(SelectedItemPath);
+            }
+            catch (BadImageFormatException)
+            {
+                //  The file is not an assembly.
+                icon = Properties.Resources.dll_native;
+            }
+            catch (Exception)
+            {
+                //  Some other eception occured, so assume we're native.
+                icon = Properties.Resources.dll_native;
+            }
+
+            //  If we haven't determined that the dll is native, use the managed icon.
+            if (icon == null)
+                icon = Properties.Resources.dll_managed;
+
+            //  Return the icon with the correct size. Use the SharpIconHandler 'GetIconSpecificSize'
+            //  function to extract the icon of the required size.
+            return GetIconSpecificSize(icon, new Size((int)iconSize, (int)iconSize));
+        }
+    }
+}
---
+++ TextIconHandler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using SharpShell.SharpIconHandler;
+using SharpShell.Attributes;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace shellhandler
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.FileExtension, ".txt")]
+    class TextIconHandler : SharpIconHandler
+    {
+        private readonly Lazy<Font> thumbnailFont;
+        private readonly Lazy<Brush> thumbnailTextBrush;
+
+        public TextIconHandler()
+        {
+            thumbnailFont = new Lazy<Font>(() => new Font("Courier New", 12f));
+            thumbnailTextBrush = new Lazy<Brush>(() => new SolidBrush(Color.Black));
+        }
+
+        protected override Icon GetIcon(bool smallIcon, uint iconSize)
+        {
+            //  Attempt to open the stream with a reader.
+            try
+            {
+                using (var reader = new StreamReader(this.SelectedItemPath))
+                {
+                    //  Read up to ten lines of text.
+                    var previewLines = new List<string>();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        var line = reader.ReadLine();
+                        if (line == null)
+                            break;
+                        previewLines.Add(line);
+                    }
+
+                    //  Now return a preview of the lines.
+                    var img = CreateThumbnailForText(previewLines, iconSize);
+
+                    Icon icon = Icon.FromHandle(img.GetHicon());
+                    return icon;
+                    //return GetIconSpecificSize(icon, new Size((int)iconSize, (int)iconSize));
+                }
+            }
+            catch (Exception exception)
+            {
+                //  Log the exception and return null for failure.
+                LogError("An exception occured opening the text file.", exception);
+                return GetIconSpecificSize(Properties.Resources.blank, new Size((int)iconSize, (int)iconSize));
+            }
+        }
+        /*protected override System.Drawing.Bitmap GetThumbnailImage(uint width)
+        {
+
+        }*/
+
+        /// <summary>
+        /// Creates the thumbnail for text, using the provided preview lines.
+        /// </summary>
+        /// <param name="previewLines">The preview lines.</param>
+        /// <param name="width">The width.</param>
+        /// <returns>
+        /// A thumbnail for the text.
+        /// </returns>
+        private Bitmap CreateThumbnailForText(IEnumerable<string> previewLines, uint width)
+        {
+            //  Create the bitmap dimensions.
+            var thumbnailSize = new Size((int)width, (int)width);
+
+            //  Create the bitmap.
+            var bitmap = new Bitmap(thumbnailSize.Width, thumbnailSize.Height, PixelFormat.Format32bppArgb);
+
+            //  Create a graphics object to render to the bitmap.
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                //  Set the rendering up for anti-aliasing.
+                graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+
+                //  Draw the page background.
+                graphics.DrawImage(Properties.Resources.blank.ToBitmap(), 0, 0, thumbnailSize.Width, thumbnailSize.Height);
+
+                //  Create offsets for the text.
+                var xOffset = width * 0.2f;
+                var yOffset = width * 0.3f;
+                var yLimit = width - yOffset;
+
+                graphics.Clip = new Region(new RectangleF(xOffset, yOffset, thumbnailSize.Width - (xOffset * 2), thumbnailSize.Height - width * .1f));
+
+                //  Render each line of text.
+                foreach (var line in previewLines)
+                {
+                    graphics.DrawString(line, thumbnailFont.Value, thumbnailTextBrush.Value, xOffset, yOffset);
+                    yOffset += 14f;
+                    if (yOffset + 14f > yLimit)
+                        break;
+                }
+            }
+
+            //  Return the bitmap.
+            return bitmap;
+        }
+    }
+}
